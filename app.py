47e9aa785e18e0e75158dab9ad652c22f07ea459
rp96536 <rp96536@gmail.com>
2024-02-13 16:22:24 +0530

    Updated code

--- app.py
+++ app.py
@@ -9,13 +9,15 @@ import os
 ## Function to load OpenAI model and get responses
 
 def get_openai_response(question):
-    llm =  OpenAI(openai_api_key=os.getenv("OPEN_API_KEY"), model_name="text-davinci-003", temperature=0.6)
+    llm =  OpenAI(openai_api_key='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', model_name="text-davinci-003", temperature=0.6)
     response = llm(question)
     return response
 
 # Initalize streamlit app
     
 st.set_page_config(page_title="Q&A Demo")
+
+# nothing to commit
 
 st.header("Langchain Application")
 
